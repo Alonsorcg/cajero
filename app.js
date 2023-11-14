9a42ff8e69ffc99b8c2989662e5d56b6b7308165
alonsorcg <alonsoraul15@gmail.com>
2023-11-14 15:11:28 -0600

    adicion de credenciales y banca en linea

---
+++ app.js
@@ -0,0 +1,11 @@
+function login(form){
+    if (form.u.value=="pedro") {
+        if (form.c.value=="123") {
+            location="principal.html";
+        } else {
+            alert("error de password");
+        }
+    } else {
+        alert("error de usuario")
+    }
+}
